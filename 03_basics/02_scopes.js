dba8b64daf33e081cbd9693a7b452c7721ec06c3
HafizIkrashUE <hafizikrash809@gmail.com>
2026-06-28 11:54:02 +0000

    scope 2

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,58 +1,55 @@
-let a = 10;
-const b = 20;
-//var c = 30;
+// let a = 10;
+// const b = 20;
+// //var c = 30;
 
-if (true) {
-    let a = 100;
-    const b = 200;
-    console.log("inner:",a);
+// if (true) {
+//     let a = 100;
+//     const b = 200;
+//     console.log("inner:",a);
     
-    //var c = 300;
-}
+//     //var c = 300;
+// }
 
-console.log(a); // 10
-console.log(b); // 20
+// console.log(a); // 10
+// console.log(b); // 20
 //console.log(c); // 300
 
 
-// new video
-
-// function one() {
-//     const username = "hitesh"
-
-//     function two() {
-//         const website = "youtube"
-//         // console.log(username);
-//         // console.log(website);
-//     }
-
-//     // two()
+// // new video
+// function one(){
+//     const username="ikrash";
+//     function two(){
+//         const website="youtube";
+//         console.log(username);
+//         console.log(website);
+//         }
+//      two()     
 // }
+// one()
 
-// // one()
 
 // if (true) {
-//     const username = "hitesh"
-//     if (username === "hitesh") {
+//     const username = "ikrash";
+//     if (username === "ikrash") {
 //         const website = "youtube"
-//         // console.log(username + website);
+//          console.log(username +website);
 //     }
-//     // console.log(website);
+//      //console.log(website);
 // }
 
-// // console.log(username);
+// console.log(username);
 
 // ++++++++++++++++++ interesting ++++++++++++++++++
+console.log(addone(3));
+function addone(num){
+    return num+1;
+}
+//console.log(addone(3));
 
-// console.log(addone(5))
+//2nd function
+//console.log(addtwo(3));
+const addtwo = function(num){
+    return num+2;
+}
+console.log(addtwo(3));
 
-// function addone(num) {
-//     return num + 1
-// }
-
-// addTwo(5)
-// const addTwo = function(num) {
-//     return num + 2
-// }
-
-*/
