0688b73410b58bb47eeadf58056e721f3e711396
HafizIkrashUE <hafizikrash809@gmail.com>
2026-07-01 16:11:07 +0500

    Completed IIFE examples

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,12 +1,24 @@
 // Immediately Invoked Function Expressions (IIFE)
 
+//It creates its own private scope, helping avoid pollution of the global scope.
 
-(function chai(){
-    // named IIFE
-    console.log(`DB CONNECTED`);
+(function greet(name){
+    let username ="ikrash";
+    console.log(`Welcome to ${username} dev`);
+})("ikrash");
+
+
+// (function DB Connected(){
+//     // named IIFE
+//     console.log(`DB CONNECTED`);
+// })();
+(() => {
+    console.log("DB Connected");
+    
 })();
 
-( (name) => {
-    console.log(`DB CONNECTED TWO ${name}`);
-} )('hitesh')
+// ( (name) => {
+//     console.log(`DB CONNECTED TWO ${name}`);
+// } )('ikrash')
+
 
