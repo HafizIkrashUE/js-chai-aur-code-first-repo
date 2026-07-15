252194a468dcf99594c0124312689049c07b76f7
HafizIkrashUE <hafizikrash809@gmail.com>
2026-07-15 15:51:11 +0000

    promises

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -1,86 +1,86 @@
-// const promiseOne = new Promise(function(resolve, reject){
-//     //Do an async task
-//     // DB calls, cryptography, network
-//     setTimeout(function(){
-//         console.log('Async task is compelete');
-//         resolve()
-//     }, 1000)
-// })
+// // const promiseOne = new Promise(function(resolve, reject){
+// //     //Do an async task
+// //     // DB calls, cryptography, network
+// //     setTimeout(function(){
+// //         console.log('Async task is compelete');
+// //         resolve()
+// //     }, 1000)
+// // })
 
-// promiseOne.then(function(){
-//     console.log("Promise consumed");
-// })
+// // promiseOne.then(function(){
+// //     console.log("Promise consumed");
+// // })
 
-// new Promise(function(resolve, reject){
-//     setTimeout(function(){
-//         console.log("Async task 2");
-//         resolve()
-//     }, 1000)
+// // new Promise(function(resolve, reject){
+// //     setTimeout(function(){
+// //         console.log("Async task 2");
+// //         resolve()
+// //     }, 1000)
 
-// }).then(function(){
-//     console.log("Async 2 resolved");
-// })
+// // }).then(function(){
+// //     console.log("Async 2 resolved");
+// // })
 
-// const promiseThree = new Promise(function(resolve, reject){
-//     setTimeout(function(){
-//         resolve({username: "Chai", email: "chai@example.com"})
-//     }, 1000)
-// })
+// // const promiseThree = new Promise(function(resolve, reject){
+// //     setTimeout(function(){
+// //         resolve({username: "Chai", email: "chai@example.com"})
+// //     }, 1000)
+// // })
 
-// promiseThree.then(function(user){
-//     console.log(user);
-// })
-const promisefour = new Promise(function(resolve, reject) {
-    setTimeout(function() {
-        let error = false;
+// // promiseThree.then(function(user){
+// //     console.log(user);
+// // })
+// // const promisefour = new Promise(function(resolve, reject) {
+// //     setTimeout(function() {
+// //         let error = false;
 
-        if (!error) {
-            resolve({
-                username: "ikrash",
-                email: "hafizikrash809@gmail.com"
-            });
-        } else {
-            reject("Something went wrong");
-        }
-    }, 1000);
-});
+// //         if (!error) {
+// //             resolve({
+// //                 username: "ikrash",
+// //                 email: "hafizikrash809@gmail.com"
+// //             });
+// //         } else {
+// //             reject("Something went wrong");
+// //         }
+// //     }, 1000);
+// // });
 
-promisefour
-.then((user) => {
-    console.log(user);
-    return user.username;
-})
-.then((username) => {
-    console.log(username);
-})
-.catch((error) => {
-    console.log(error);
-})
-.finally(() => {
-    console.log("Promise is either resolved or rejected.");
-});
+// // promisefour
+// // .then((user) => {
+// //     console.log(user);
+// //     return user.username;
+// // })
+// // .then((username) => {
+// //     console.log(username);
+// // })
+// // .catch((error) => {
+// //     console.log(error);
+// // })
+// // .finally(() => {
+// //     console.log("Promise is either resolved or rejected.");
+// // });
 
-// const promiseFive = new Promise(function(resolve, reject){
-//     setTimeout(function(){
-//         let error = true
-//         if (!error) {
-//             resolve({username: "javascript", password: "123"})
-//         } else {
-//             reject('ERROR: JS went wrong')
-//         }
-//     }, 1000)
-// });
+// // const promiseFive = new Promise(function(resolve, reject){
+// //     setTimeout(function(){
+// //         let error = true
+// //         if (!error) {
+// //             resolve({username: "javascript", password: "123"})
+// //         } else {
+// //             reject('ERROR: JS went wrong')
+// //         }
+// //     }, 1000)
+// // });
 
-// async function consumePromiseFive(){
-//     try {
-//         const response = await promiseFive
-//         console.log(response);
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
+// // async function consumePromiseFive(){
+// //     try {
+// //         const response = await promiseFive
+// //         console.log(response);
+// //     } catch (error) {
+// //         console.log(error);
+// //     }
+// // }
 
-// consumePromiseFive()
+// // consumePromiseFive()
 
 // // async function getAllUsers(){
 // //     try {
@@ -93,9 +93,8 @@ promisefour
 // //     }
 // // }
 
-// //getAllUsers()
-
-// fetch('https://api.github.com/users/hiteshchoudhary')
+// // getAllUsers()
+// fetch('https://api.github.com/users/HafizIkrashUE')
 // .then((response) => {
 //     return response.json()
 // })
@@ -104,5 +103,5 @@ promisefour
 // })
 // .catch((error) => console.log(error))
 
-// // promise.all
-// // yes this is also available, kuch reading aap b kro.
+// // // promise.all
+// // // yes this is also available, kuch reading aap b kro.
