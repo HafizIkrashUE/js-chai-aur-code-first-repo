573e4fa6de6b6e15f150dc48e7d03fcb3d1a2580
HafizIkrashUE <hafizikrash809@gmail.com>
2026-07-21 11:11:26 +0000

    getter setter

--- 10_classes_and_oop/getter_setter.js
+++ 10_classes_and_oop/getter_setter.js
@@ -1,24 +1,24 @@
-class User {
+class User { 
     constructor(email, password){
         this.email = email;
         this.password = password
     }
-
+    get password(){
+        return this._password.toUpperCase();
+    }
+    set password(value){
+        this._password = value;
+    }
     get email(){
-        return this._email.toUpperCase()
+        return this._email.toUpperCase();
     }
     set email(value){
-        this._email = value
+        this._email = value;
     }
 
-    get password(){
-        return `${this._password}hitesh`
-    }
 
-    set password(value){
-        this._password = value
-    }
+
 }
-
-const hitesh = new User("h@hitesh.ai", "abc")
-console.log(hitesh.email);
+const goodvariable = new User("ikrash@example.com", "abc");
+console.log(ikrash.password);
+console.log(ikrash.email);
